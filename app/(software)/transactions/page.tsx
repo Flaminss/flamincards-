7635560f81a09dba703f0785702100ec1cb4c473
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-06-04 21:43:45 -0700

    feat: add new analytical page

--- app/(software)/transactions/page.tsx
+++ app/(software)/transactions/page.tsx
@@ -1,1 +1,15 @@
-export { ComingSoonScene as default } from "@/app/coming-soon-scene";
+export default function TransactionsPage() {
+  return (
+    <div>
+      <header className="py-5 md:pt-0 lg:pt-1 px-4 mb-2">
+        <h1 className="text-xl md:text-2xl lg:text-3xl">Transaction History</h1>
+      </header>
+      
+      <section className="px-4 py-6">
+        <div>
+            
+        </div>
+      </section>
+    </div>
+  );
+}
