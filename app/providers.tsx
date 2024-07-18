df97fda95266ee0f1eb0f55d6a5b56ec499d2c2a
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-18 14:38:28 -0700

    fix: add expected import path

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import * as React from "react";
-import { NextUIProvider } from "@nextui-org/system";
+import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+
 export interface ProvidersProps {
   children: React.ReactNode;
   themeProps?: ThemeProviderProps;
@@ -17,6 +18,7 @@ export function Providers({ children, themeProps }: ProvidersProps) {
     // <NextUIProvider navigate={router.push}>
     //   <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
     // </NextUIProvider>
+
     <NextThemesProvider {...themeProps}>
       <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
     </NextThemesProvider>
