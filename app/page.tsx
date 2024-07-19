ceb866fa3b005d3fc8fb5a14a6b14bab400dd037
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-19 10:02:10 -0700

    feat: style top navigation

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,14 @@ import {
 } from "@/lib/icons";
 
 import { Logo } from "@/lib/icons";
+import { BrandName } from "./(pwa)/navigation-top";
+import {
+  InstagramIcon,
+  Music2Icon,
+  PhoneIcon,
+  SendHorizontalIcon,
+} from "lucide-react";
+import { Chip } from "@nextui-org/react";
 
 export default function HomePage() {
   return (
@@ -122,73 +130,17 @@ export const TopNavigation = () => {
   );
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
+    <NextUINavbar maxWidth="lg" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
-          </NextLink>
-        </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </ul>
-      </NavbarContent>
-
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.twitter} aria-label="Twitter">
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.discord} aria-label="Discord">
-            <DiscordIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.github} aria-label="Github">
-            <GithubIcon className="text-default-500" />
-          </Link>
-          <ThemeSwitch />
-        </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Sponsor
-          </Button>
-        </NavbarItem>
+        <BrandName />
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github} aria-label="Github">
-          <GithubIcon className="text-default-500" />
-        </Link>
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
@@ -209,6 +161,41 @@ export const TopNavigation = () => {
           ))}
         </div>
       </NavbarMenu>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full items-center"
+        justify="end"
+      >
+        <NavbarItem className="hidden md:flex">
+          <NextLink href="/dashboard">Dashboard</NextLink>
+        </NavbarItem>
+        <NavbarItem className="hidden md:flex">
+          <NextLink href="gistnaija.com">Blog</NextLink>
+        </NavbarItem>
+        <NavbarItem className="hidden md:flex">
+          <Button as={Link} variant="light">
+            Contact
+          </Button>
+        </NavbarItem>
+        <NavbarItem className="hidden md:flex">
+          <Button
+            isExternal
+            as={Link}
+            className="text-sm font-normal text-default-600 bg-default-100"
+            href={siteConfig.links.sponsor}
+            startContent={
+              <HeartFilledIcon className="text-sm size-4 text-danger" />
+            }
+            variant="flat"
+            size="md"
+          >
+            Sponsor
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <ThemeSwitch />
+        </NavbarItem>
+      </NavbarContent>
     </NextUINavbar>
   );
 };
