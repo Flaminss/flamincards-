d9a61d75ef6bd7d3dcc95e167a11edf196efc2c7
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-22 16:07:30 -0700

    feat: remove peer dependency error prone package

--- app/(pwa)/(marketplace)/crypto/page.tsx
+++ app/(pwa)/(marketplace)/crypto/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import MultiStep from "react-multistep";
+// import MultiStep from "react-multistep";
 import CryptoSalePitchStep from "./pitch-step";
 import CryptoSalePaymentStep from "./payment-step";
 import { Button } from "@nextui-org/button";
@@ -8,12 +8,12 @@ import { Button } from "@nextui-org/button";
 export default function CryptoSellPage() {
   return (
     <>
-      <MultiStep
+      {/* <MultiStep
         prevButton={{ title: "Go Back", style: { paddingBlock: "2rem" } }}
       >
-        <CryptoSalePitchStep title="Provide Information" />
-        <CryptoSalePaymentStep title="Payment Procedure" />
-      </MultiStep>
+      </MultiStep> */}
+      <CryptoSalePitchStep title="Provide Information" />
+      <CryptoSalePaymentStep title="Payment Procedure" />
     </>
   );
 }
