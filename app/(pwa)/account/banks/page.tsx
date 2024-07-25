5fc41bb5ad8550248526c7ade87881171c194ad3
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-25 11:00:42 -0700

    feat: add page to render user bank collection

---
+++ app/(pwa)/account/banks/page.tsx
@@ -0,0 +1,11 @@
+export default function Banks() {
+  return (
+    <div className="pb-20">
+      <header className="py-5 md:pt-0 lg:pt-1 px-4 mb-2">
+        <h1 className="text-2xl lg:text-3xl">Banks</h1>
+      </header>
+
+      <div className="py-10">List of bank informations</div>
+    </div>
+  );
+}
