894ad357de076ad3b14fc3f8e052467f2169f2a0
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-26 09:59:28 -0700

    feat: add component to provide quick wallet action

---
+++ app/(pwa)/dashboard/quick-wallet-actions.tsx/index.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { Button, ButtonGroup } from "@nextui-org/react";
+import { CircleDollarSignIcon, DownloadIcon, Handshake } from "lucide-react";
+
+export default function QuickWalletActions() {
+  return (
+    <ButtonGroup
+      className="mt-auto.. items-start flex gap-1 flex-wrap content-end shadow-lg"
+      variant="solid"
+      // variant="light"
+      color="primary"
+      size="md"
+    >
+      <Button
+        as={Link}
+        href="/funding"
+        className="shadow-lg grow font-medium !rounded-md xl:!rounded-s-lg xl:!rounded-e-none"
+        startContent={<DownloadIcon className="size-4" />}
+        // className="shadow-lg grow font-medium !rounded-md xl:!rounded-s-lg xl:rounded-e-none flex-col h-20"
+        // startContent={<DownloadIcon className="size-4" className="lg:size-8 flex-shrink-0" />}
+      >
+        Deposit
+      </Button>
+      <Button
+        as={Link}
+        href="/withdrawl"
+        className="shadow-lg grow font-medium !rounded-md xl:!rounded-none"
+        startContent={<CircleDollarSignIcon className="size-4" />}
+      >
+        Withdraw
+      </Button>
+      <Button
+        as={Link}
+        href="/dashboard#investment-portfolio"
+        className="shadow-lg grow font-medium !rounded-md xl:!rounded-e-lg xl:!rounded-s-none"
+        startContent={<Handshake className="size-4" />}
+      >
+        Invest
+      </Button>
+    </ButtonGroup>
+  );
+}
