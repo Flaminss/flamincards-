c0ddcee0e08d83c21a41cec2ab2112ce1801b211
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-26 12:08:44 -0700

    feat: massive simplistic redesign of content and entisement cards

--- app/(pwa)/dashboard/giftcard-entisement-section.tsx
+++ app/(pwa)/dashboard/giftcard-entisement-section.tsx
@@ -1,20 +1,27 @@
-import { Button, Chip, Link } from "@nextui-org/react";
-import { ArrowRight } from "lucide-react";
-import Image from "next/image";
+import { Button, Chip, Link, Image } from "@nextui-org/react";
+import { ArrowRight, ChevronsRight } from "lucide-react";
 import icons from "currency-icons";
+import clsx from "clsx";
 
 export default function GiftCardEntisementSection() {
   return (
     <div>
       <section className="py-6">
         <header className="px-4 flex justify-between items-center gap-2 mb-4">
-          <h2 className="text-lg font-semibold">Redeem/Sell Giftcards</h2>
-          <Button variant="light" color="primary" className="pe-0">
+          <h2 className="text-xl lg:text-2xl">Sell Your Giftcards</h2>
+          <Button
+            as={Link}
+            href="/giftcards"
+            variant="light"
+            underline
+            color="primary"
+            className="pe-0"
+          >
             View All
           </Button>
         </header>
         <div className="px-4 mb-8">
-          <ul className="flex flex-wrap overflow-x-auto items-start gap-4">
+          <ul className="grid grid-cols-2 lg:grid-cols-3 overflow-x-auto items-start gap-4">
             {[
               {
                 name: "Steam",
@@ -49,7 +56,7 @@ export default function GiftCardEntisementSection() {
                 },
                 tag: {
                   title: "Low Rate",
-                  status: "success",
+                  status: "warning",
                 },
               },
               {
@@ -60,52 +67,49 @@ export default function GiftCardEntisementSection() {
                   to: "USD",
                 },
                 tag: {
-                  title: "Low Rate",
-                  status: "success",
+                  title: "Common",
+                  status: "primary",
                 },
               },
             ].map(({ name, rate, tag }, index) => {
               return (
-                <li
+                <Link
                   key={`${index}#${tag}`}
-                  className="border rounded-lg px-3 py-2.5 min-h-12 grow max-w-[50%] min-w-32"
+                  href={`/giftcards/${name}`}
+                  className="block text-white shadow rounded-sm p-4 border border-zinc-800"
                 >
-                  <div className="relative">
-                    <Image
-                      src=""
-                      alt=""
-                      className="dark:bg-gray-900 h-20 mb-2 rounded-sm border-none w-full"
-                    />
-                    <Chip
-                      variant="solid"
-                      color={tag.status as any}
-                      radius="none"
-                      size="sm"
-                      className="absolute top-1 right-1"
+                  <h5 className="text-medium lg:text-lg flex justify-between mb-1">
+                    {name}{" "}
+                    <span
+                      className={clsx(
+                        "text-xs leading-6 font-medium px-2 rounded-xl",
+                        `text-${tag.status} bg-${tag.status}-50`
+                      )}
                     >
                       {tag.title}
-                    </Chip>
+                    </span>
+                  </h5>
+                  <div className="flex justify-between gap-x-4">
+                    <p className="text-success-400 text-sm lg:text-base font-medium flex items-end gap-x-1 align-baseline">
+                      <span>{`${icons[rate.from]?.symbol} ${rate.value}/${
+                        icons[rate.to]?.symbol
+                      }`}</span>
+                    </p>
+
+                    <Button
+                      isIconOnly
+                      variant="light"
+                      size="sm"
+                      className="h-auto"
+                    >
+                      <ChevronsRight className="size-4 lg:size-5" />
+                    </Button>
                   </div>
-                  <h5 className="text-base font-medium">{name}</h5>
-                  <p className="text-success-400 text-sm flex items-end gap-x-1 align-baseline">
-                    <span>🔥</span>
-                    <span>{`${icons[rate.from]?.symbol}${rate.value}/${
-                      icons[rate.to]?.symbol
-                    }`}</span>
-                  </p>
-                </li>
+                </Link>
               );
             })}
           </ul>
         </div>
-        <Link
-          color="primary"
-          size="lg"
-          underline="always"
-          className="mx-auto flex gap-x-2 items-center w-max"
-        >
-          See more cards at the market <ArrowRight size={18} />
-        </Link>
       </section>
     </div>
   );
