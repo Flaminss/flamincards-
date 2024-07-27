52228d1d69e736b0eab39ff16bc67a5e35549143
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-27 14:26:30 -0700

    refactor: redesign for visual aesthetics

--- app/(pwa)/dashboard/investment-portfolio-section.tsx
+++ app/(pwa)/dashboard/investment-portfolio-section.tsx
@@ -13,18 +13,20 @@ import {
   CardBody,
 } from "@nextui-org/react";
 import { AlertCircle } from "lucide-react";
+import PWASectionTitle from "../section-title";
 
 export default function InvestmentPortfolioSection() {
   return (
     <section className="px-4 py-8" id="investment-portfolio">
-      <header className="mb-6 space-y-2">
-        <h2 className="text-xl lg:text-2xl">Your Investments Portfolio</h2>
-        <p className="text-sm text-warning-500 leading-snug flex items-center gap-x-4 sm:gap-x-2">
-          <AlertCircle className="size-8 sm:size-4" />{" "}
-          <p>
+      <header className="mb-6 space-y-4">
+        <PWASectionTitle title="Your Investments Portfolio" />
+        <p className="text-sm text-warning-500 leading-snug flex items-center gap-x-3 sm:gap-x-2">
+          <span className="p-2 rounded-lg bg-warning-50">
+            <AlertCircle className="size-5" />{" "}
+          </span>
+          <p className="max-w-[28ch]">
             Choose a Plan - You have{" "}
-            <span className="font-medium text-white">0 of 2</span> investment
-            running.
+            <span className="px-1 text-white">0 of 2</span> investment running.
           </p>
         </p>
       </header>
@@ -33,12 +35,12 @@ export default function InvestmentPortfolioSection() {
         {[{ title: "Basic" }, { title: "Silver" }, { title: "Gold" }].map(
           ({ title }, index) => {
             return (
-              <li key={title} className="grow max-w-72..">
-                <Card className="p-2 shadow-lg border">
-                  <CardHeader className="text-lg font-bold py-2 shadow-lg bg-yellow-100 text-yellow-800 text-center justify-center">
+              <li key={title} className="grow min-w-[24ch].. max-w-72..">
+                <Card className="shadow p-2">
+                  <CardHeader className="text-medium py-3 shadow-lg bg-default font-medium uppercase text-center justify-center">
                     {title}
                   </CardHeader>
-                  <CardBody className="pt-8">
+                  <CardBody className="pt-8 px-2">
                     <div className="space-y-1.5">
                       <p className="flex justify-between">
                         Capital:{" "}
