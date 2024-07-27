d3d1185bc1b98055f304ce74c3997eadabd2fbf3
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-27 14:27:19 -0700

    feat: reduce visual noice of insignificant ui component

--- app/(pwa)/dashboard/primary-bank-card.tsx
+++ app/(pwa)/dashboard/primary-bank-card.tsx
@@ -33,13 +33,13 @@ export default function PrimaryBankCard() {
 
   return (
     <div className="grid gap-y-1 max-w-md w-full mx-auto">
-      <article className="relative border border-slate-800.. shadow-md p-4 sm:p-5 lg:p-4">
-        <div className="flex gap-x-2.5 items-center mb-2">
+      <article className="relative border shadow p-4 sm:p-5 lg:p-4">
+        <div className="flex gap-x-4 items-end mb-2">
           <div className="inline-flex items-center">
-            <div className="h-4 w-4 rounded-full bg-warning-400"></div>
-            <div className="h-4 w-4 rounded-full bg-danger-400 -ms-1.5"></div>
+            <div className="h-4 w-4 rounded-full bg-warning"></div>
+            <div className="h-4 w-4 rounded-full bg-danger -ms-1.5"></div>
           </div>
-          <span className="text-sm text-zinc-400 uppercase font-mono">
+          <span className="text-xs text-zinc-400 font-mono">
             Primary Bank
           </span>
         </div>
@@ -53,7 +53,7 @@ export default function PrimaryBankCard() {
           color="success"
           size="sm"
           radius="sm"
-          className="text-xs absolute top-2 right-2"
+          className="text-xs absolute top-2 right-2 min-w-[auto]"
           onClick={() => edit()}
         >
           Edit
