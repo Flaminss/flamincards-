2c4f7674278409d136899803e7ab2726fcbdb88e
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-28 16:26:15 -0700

    refactor keep consistent use of terminology in filenames

--- app/(pwa)/notifications/transaction-reads.tsx
+++ app/(pwa)/notifications/transaction-reads.tsx
@@ -10,8 +10,9 @@ import {
   Button,
 } from "@nextui-org/react";
 import { CircleArrowRightIcon } from "lucide-react";
+import ReadFooter from "./read-footer";
 
-export default function PaymentReads() {
+export default function TransactionReads() {
   return (
     <div className="grid gap-y-6 px-4">
       {[1, 2, 3, 4].map((count) => {
@@ -20,7 +21,9 @@ export default function PaymentReads() {
             <CardHeader className="justify-between items-start">
               <div className="flex gap-x-2">
                 🎁
-                <h4 className="font-semibold text-lg">Gift Card</h4>
+                <h4 className="font-semibold lg:text-lg text-primary">
+                  Gift Card
+                </h4>
               </div>
               <Chip
                 color="success"
@@ -31,21 +34,16 @@ export default function PaymentReads() {
                 Success
               </Chip>
             </CardHeader>
-            <CardBody className="justify-center items-center py-4 gap-y-4">
-              <p className="text-lg">Steam ($2000) x{count}</p>
+            <CardBody className="justify-center items-center py-4 gap-y-4 pb-6">
+              <p className="lg:text-lg font-mono">Steam ($2000) x {count}</p>
               <p className="text-center text-sm">
                 <span className="text-success-400">You Recieved:</span>{" "}
-                <span className="block text-4xl font-medium">N10,000</span>
+                <span className="block text-3xl lg:text-4xl font-medium">
+                  N10,000
+                </span>
               </p>
             </CardBody>
-            <CardFooter className="justify-between items-center">
-              <p className="text-primary-900 text-sm px-2">
-                May 02, 2023 at 9:30pm
-              </p>
-              <Button size="md" variant="light">
-                View Details <CircleArrowRightIcon size={16} />
-              </Button>
-            </CardFooter>
+            <ReadFooter />
           </Card>
         );
       })}
