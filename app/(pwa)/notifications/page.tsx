78c0f0c0867fb99d60866386e71e87a755830219
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-28 16:27:28 -0700

    refactor: update design fore responsive aesthetics

--- app/(pwa)/notifications/page.tsx
+++ app/(pwa)/notifications/page.tsx
@@ -1,21 +1,21 @@
 "use client";
 
 import { Tabs, Tab } from "@nextui-org/react";
-import PaymentReads from "./payment-reads";
+import TransactionReads from "./transaction-reads";
 import InboxReads from "./inbox-reads";
-import ActivityReads from "./activity-reads";
+import CampaignReads from "./campaign-reads";
 import PWAPageTitle from "../page-title";
 
+const menu = [
+  { title: "Transactions", unread: 0, render: () => <TransactionReads /> },
+  { title: "Messages", unread: 0, render: () => <InboxReads /> },
+  { title: "Promotions", unread: 4, render: () => <CampaignReads /> },
+];
+
 export default function NotificationsPage() {
-  const menu = [
-    { title: "Inbox", unread: 0, render: () => <InboxReads /> },
-    { title: "Payments", unread: 0, render: () => <PaymentReads /> },
-    { title: "News & Giveaways", unread: 4, render: () => <ActivityReads /> },
-  ];
   return (
     <div>
       <header className="py-5 md:pt-0 lg:pt-1 px-4 mb-2">
-        {/* <h1 className="text-xl md:text-2xl lg:text-3xl">Notifications</h1> */}
         <PWAPageTitle title="Notifications" />
       </header>
 
@@ -26,7 +26,7 @@ export default function NotificationsPage() {
         className="mb-6"
         classNames={{
           base: "px-2",
-          tab: "text-lg",
+          tab: "sm:text-lg",
           panel: "px-0",
         }}
       >
@@ -38,7 +38,7 @@ export default function NotificationsPage() {
                 <div className="flex items-center">
                   {title}{" "}
                   {!unread ? null : (
-                    <span className="ms-2 badge badge-flat-primary">
+                    <span className="ms-2 badge badge-sm badge-flat-danger">
                       {unread}
                     </span>
                   )}
