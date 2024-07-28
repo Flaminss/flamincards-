5f69a8571a07242042d7163bcfcfc10574fb8214
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-28 16:23:50 -0700

    refactor: be more direct about component scope via filename

--- app/(pwa)/notifications/campaign-reads.tsx
+++ app/(pwa)/notifications/campaign-reads.tsx
@@ -11,13 +11,14 @@ import {
   Button,
 } from "@nextui-org/react";
 import { CircleArrowRightIcon, Share2Icon } from "lucide-react";
+import ReadFooter from "./read-footer";
 
-export default function ActivityReads() {
+export default function CampaignReads() {
   return (
-    <div className="grid gap-y-6 px-4">
+    <div className="grid grid-cols-2 gap-4 px-4">
       {[1, 2, 3, 4].map((count) => {
         return (
-          <Card key={count} className="max-w-[64ch] lg:w-[64ch] shadow">
+          <Card key={count} className="max-w-[64ch] lg:max-w-[64ch] shadow">
             <CardHeader>
               <Link href="/">
                 <Image
@@ -25,7 +26,7 @@ export default function ActivityReads() {
                   radius="lg"
                   width="100%"
                   alt=""
-                  className="w-full object-cover h-[196px]"
+                  className="w-full object-cover h-[140px] lg:h-[196px]"
                   src=""
                 />
               </Link>
@@ -34,39 +35,25 @@ export default function ActivityReads() {
                 radius="full"
                 size="md"
                 isIconOnly
-                className="absolute top-2 right-2 shadow-md"
+                className="absolute top-2 right-2 shadow"
               >
-                <Share2Icon className="text-sm" size={18} />
+                <Share2Icon className="size-5" />
               </Button>
             </CardHeader>
             <CardBody className="px-4">
               <header className="flex items-center gap-x-3 mb-3">
-                <Chip
-                  size="lg"
-                  radius="sm"
-                  className="text-xl"
-                  classNames={{ base: "p-2 h-auto", content: "p-0" }}
-                >
-                  🎉
-                </Chip>
-                <h4 className="text-ellipsis overflow-hidden max-h-20 leading-snug text-xl max-w-[38ch]">
-                  You are one step away from becoming a Millionaire 🤑
+                🤑
+                <h4 className="text-ellipsis overflow-hidden max-h-20 leading-snug font-medium lg:text-lg max-w-[46ch]">
+                  You are one step away from becoming a Millionaire
                 </h4>
               </header>
-              <p>
+              <p className="text-sm text-zinc-100 lg:text-base">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
                 doloremque laudantium expedita, deleniti necessitatibus aliquid
                 inventore blanditiis amet? Ipsa, ab.
               </p>
             </CardBody>
-            <CardFooter className="justify-between items-center">
-              <p className="text-primary-800 text-sm px-2">
-                May 02, 2023 at 9:30pm
-              </p>
-              <Button size="md" variant="light">
-                View Details <CircleArrowRightIcon size={16} />
-              </Button>
-            </CardFooter>
+            <ReadFooter />
           </Card>
         );
       })}
