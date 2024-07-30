f4bbac63331be5687f670b6751b4df44f65d6080
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-29 23:59:57 -0700

    feat: redesign page for more simple and aesthetic look

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,92 +1,83 @@
 "use client";
 
-import { Button, Checkbox, DatePicker, Input } from "@nextui-org/react";
-import TopNavigation, { BrandName } from "../(pwa)/navigation-top";
-import PWASectionTitle from "../(pwa)/section-title";
-import Link from "next/link";
 import { UserCircle } from "lucide-react";
+import { Button, Checkbox, Input, Link } from "@nextui-org/react";
+import AuthFlowNavigationTop from "../(pwa)/auth-flow-navigation-top";
 
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex flex-col bg-black">
-      <section className="navbar navbar-glass border-b w-full px-0">
-        <div className="flex max-w-lg w-full flex-grow mx-auto px-4 items-center">
-          <BrandName />
+      <AuthFlowNavigationTop
+        renderCTA={() => (
           <Button
             as={Link}
-            variant="flat"
-            color="primary"
-            size="md"
+            color="default"
+            size="sm"
+            radius="sm"
             href="/register"
-            className="ms-auto"
           >
             Register
           </Button>
-        </div>
-      </section>
+        )}
+      />
 
       <main>
-        <section className="py-10 px-4 max-w-lg mx-auto">
-          <header className="mb-6">
-            <UserCircle className="block mx-auto size-10 mb-2"/>
-            <h1 className="text-xl lg:text-3xl text-center mb-2 font-medium">
+        <section className="pt-12 py-8 px-4 max-w-lg mx-auto">
+          <header>
+            <UserCircle className="block mx-auto size-10 mb-2.5 sm:size-12 sm:mb-4" />
+            <h1 className="text-2xl sm:text-3xl text-center mb-1 sm:mb-2 font-medium">
               Welcome Back! 👋
             </h1>
             <p className="text-sm text-zinc-400 text-center">
-              Please enter your login details below.
+              Provide your login details below to get started.
             </p>
           </header>
 
-          <form className="pt-6">
-            <div className="grid gap-y-10">
+          <form className="pt-10">
+            <div className="grid gap-y-6 mb-10">
+              <Input radius="sm" variant="flat" label="Email" />
               <Input
-                size="lg"
-                radius="sm"
-                variant="flat"
-                label="Email"
-                labelPlacement="outside"
-                placeholder=" "
-                fullWidth
-              />
-              <Input
-                size="lg"
                 radius="sm"
                 variant="flat"
                 type="password"
-                id="login-password"
-                label={
-                  <>
-                    <label htmlFor="login-password">Password</label>
-                    <Link
-                      href="/recover-password"
-                      className="text-medium text-primary"
-                    >
-                      Forgot password
-                    </Link>
-                  </>
-                }
-                classNames={{
-                  label: "flex justify-between gap-x-4 items-end w-full",
-                }}
-                labelPlacement="outside"
-                placeholder=" "
-                fullWidth
+                label="Password"
               />
-              <Checkbox size="md" radius="sm" color="primary">
-                Remember me for 30 days
-              </Checkbox>
+              <div className="flex justify-between items-center gap-x-6">
+                <Checkbox
+                  size="md"
+                  radius="full"
+                  color="primary"
+                  classNames={{ label: "text-zinc-200" }}
+                >
+                  Remember me
+                </Checkbox>
+                <Link
+                  href="/recover-password"
+                  className="text-medium font-medium text-primary"
+                >
+                  Forgot password
+                </Link>
+              </div>
             </div>
 
-            <footer className="pt-8">
+            <footer className="grid gap-y-2">
               <Button
                 color="primary"
                 variant="solid"
                 fullWidth
                 radius="sm"
                 size="lg"
+                className="mb-2"
               >
                 Login
               </Button>
+
+              <p className="text-center text-sm">
+                Dont have an account yet?{" "}
+                <Link href="/register" color="primary" size="sm">
+                  Register
+                </Link>
+              </p>
             </footer>
           </form>
         </section>
