65c7b8dd67253903536c01888d75237bb592f9ad
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-30 00:00:17 -0700

    feat: redesign page for aesthetics and scafolded functionality

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,162 +2,95 @@
 
 import Link from "next/link";
 import { Button, DatePicker, Input } from "@nextui-org/react";
-import TopNavigation, { BrandName } from "../(pwa)/navigation-top";
-import PWASectionTitle from "../(pwa)/section-title";
+import TransactionPINInput from "../(pwa)/account/transaction-pin-input";
+import AuthFlowNavigationTop from "../(pwa)/auth-flow-navigation-top";
+import { MailPlus } from "lucide-react";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function RegisterPage() {
+  const [eligible, setEligible] = useState(false);
+
   return (
     <div className="min-h-screen flex flex-col bg-black">
-      <section className="navbar navbar-glass border-b w-full px-0">
-        <div className="flex max-w-xl w-full flex-grow mx-auto px-4 items-center">
-          <BrandName />
-          <Button
-            as={Link}
-            variant="flat"
-            color="primary"
-            size="sm"
-            href="/login"
-            className="ms-auto"
-          >
+      <AuthFlowNavigationTop
+        renderCTA={() => (
+          <Button as={Link} color="default" size="sm" radius="sm" href="/login">
             Login
           </Button>
-        </div>
-      </section>
+        )}
+      />
 
       <main>
-        <section className="py-8 px-4 max-w-xl mx-auto">
-          <header className="mb-6">
-            <h1 className="text-xl lg:text-3xl text-center font-medium">
-              Create an Account
+        <section className="pt-12 py-8 px-4 max-w-lg mx-auto">
+          <header>
+            <MailPlus className="block mx-auto size-8 mb-2 sm:size-10" />
+            <h1 className="text-2xl sm:text-3xl text-center mb-1 sm:mb-2 font-medium">
+              Create an account! ✨
             </h1>
+            <p className="text-sm text-zinc-400 text-center">
+              Start your exciting journey with us today.
+            </p>
           </header>
 
-          <form className="hidden">
-            <div className="grid gap-y-8">
-              <div className="flex flex-wrap gap-y-8 gap-x-6 w-full pt-10">
-                <div className="grow">
-                  <Input
-                    size="lg"
-                    radius="sm"
-                    variant="bordered"
-                    label="First Name"
-                    labelPlacement="outside"
-                    placeholder=" "
-                    fullWidth
-                  />
-                </div>
-                <div className="grow">
-                  <Input
-                    size="lg"
-                    radius="sm"
-                    variant="bordered"
-                    label="Last Name"
-                    labelPlacement="outside"
-                    placeholder=" "
-                    fullWidth
-                  />
-                </div>
-              </div>
+          <form className={clsx("pt-10", { hidden: eligible })}>
+            <div className="grid gap-y-6 mb-10">
+              <Input
+                isRequired
+                size="md"
+                type="email"
+                radius="sm"
+                label="Email"
+              />
+              <DatePicker
+                isRequired
+                radius="sm"
+                label="Date of Birth"
+                showMonthAndYearPickers
+              />
+              <Input radius="sm" type="password" label="Password" />
+              <Input radius="sm" type="password" label="Re-type Password" />
+            </div>
 
-              <div>
-                <Input
-                  radius="sm"
-                  size="lg"
-                  type="email"
-                  variant="bordered"
-                  label="Email"
-                  labelPlacement="outside"
-                  placeholder=" "
-                />
+            <Button
+              color="primary"
+              variant="solid"
+              fullWidth
+              radius="sm"
+              size="lg"
+              onClick={() => setEligible(true)}
+            >
+              Setup your Profile
+            </Button>
+          </form>
+
+          <form className={clsx("pt-10", { hidden: !eligible })}>
+            <div className="grid gap-y-8">
+              <div className="flex flex-wrap sm:flex-nowrap gap-4 w-full">
+                <Input size="md" radius="sm" label="First Name" />
+                <Input size="md" radius="sm" label="Last Name" />
               </div>
               <div>
-                <DatePicker
-                  size="lg"
-                  radius="sm"
-                  variant="bordered"
-                  label="Date of Birth"
-                  labelPlacement="outside"
-                  showMonthAndYearPickers
-                />
+                <label htmlFor="transaction-pin" className="text-sm block mb-2">
+                  Transaction PIN
+                </label>
+                <TransactionPINInput />
               </div>
             </div>
 
-            <footer className="pt-8">
-              <Button
-                color="primary"
-                variant="solid"
-                fullWidth
-                radius="sm"
-                size="lg"
-              >
-                Set-Up Passwords
-              </Button>
-            </footer>
+            <Button
+              as={Link}
+              color="primary"
+              variant="solid"
+              fullWidth
+              radius="sm"
+              size="lg"
+            >
+              Done! Proceed to Dashboard
+            </Button>
           </form>
-
-          <div className="hidden..">
-            <SetupPassword />
-          </div>
         </section>
       </main>
     </div>
-  );
-}
-
-function SetupPassword() {
-  return (
-    <form>
-      <div className="grid gap-y-8">
-        <div>
-          <Input
-            radius="sm"
-            variant="bordered"
-            size="lg"
-            label="Password"
-            labelPlacement="outside"
-            placeholder=" "
-          />
-        </div>
-        <div>
-          <Input
-            radius="sm"
-            variant="bordered"
-            size="lg"
-            label="Re-type Password"
-            labelPlacement="outside"
-            placeholder=" "
-          />
-        </div>
-        <div>
-          <label htmlFor="transaction-pin" className="text-sm block mb-2">
-            Transaction PIN
-          </label>
-          <div className="flex gap-x-6">
-            {[1, 2, 3, 4].map((digit, index) => {
-              return (
-                <Input 
-                key={`${digit}#${index}`}
-                  type="number"
-                  size="lg"
-                  minLength={1}
-                  maxLength={1}
-                  min="0"
-                  max="9"
-                  variant="bordered"
-                  className="lg:size-16 thin-scrollbar"
-                  radius="sm"
-                />
-              );
-            })}
-          </div>
-        </div>
-      </div>
-
-      <footer className="pt-8">
-        <Button color="primary" variant="solid" fullWidth radius="sm" size="lg">
-          Register
-        </Button>
-      </footer>
-    </form>
   );
 }
