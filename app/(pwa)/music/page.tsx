b2df2e95dbab23ddfd9f5e87ea99d996cf117c13
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-07-31 16:28:36 -0700

    feat: add scafolding of future feature implementation

--- app/(pwa)/music/page.tsx
+++ app/(pwa)/music/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Tab, Tabs } from "@nextui-org/react";
+import { Button, Link, Tab, Tabs } from "@nextui-org/react";
 import { ComingSoonScene } from "@/app/coming-soon-scene";
 import PWAPageTitle from "../page-title";
 import BeatsCatalogPage from "./beats-page";
+import { ArrowRightCircle } from "lucide-react";
 
 const musicLibraryTabs = [
   {
@@ -21,8 +22,19 @@ const musicLibraryTabs = [
 export default function MusicPage({ children }: { children: any }) {
   return (
     <div className="pb-20 max-w-xl lg:max-w-[unset] mx-auto">
+      {/* <header className="py-5 md:pt-0 lg:pt-1 px-4 mb-2 flex items-center flex-col gap-y-4 lg:flex-row justify-between flex-wrap gap-x-6"> */}
       <header className="py-5 md:pt-0 lg:pt-1 px-4 mb-2">
         <PWAPageTitle title="Music Store" />
+
+        {/* <Button
+          variant="light"
+          color="primary"
+          size="md"
+          className="font-medium h-auto py-2"
+          endContent={<ArrowRightCircle className="size-4" />}
+        >
+          My Downloads
+        </Button> */}
       </header>
 
       <BeatsCatalogPage />
