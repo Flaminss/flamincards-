9e6e02a110eec26e35c08eb6cd65464ad49b9f58
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-08-03 14:47:06 -0700

    feat: update end result ui and overall aesthetics

--- app/(pwa)/withdrawl/page.tsx
+++ app/(pwa)/withdrawl/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import Link from "next/link";
-import { Button, Input, useDisclosure } from "@nextui-org/react";
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
 import {
   CheckCircleIcon,
+  ClipboardSignature,
   DeleteIcon,
   LandmarkIcon,
   PenBoxIcon,
+  TimerIcon,
   WalletIcon,
 } from "lucide-react";
 import icons from "currency-icons";
@@ -16,41 +26,34 @@ import PWAPageTitle from "../page-title";
 
 export default function WithdrawlPage() {
   const {
-    isOpen: withdrawing,
-    onOpen: startWithdrawlProcess,
-    onOpenChange: onWithdraw,
-    onClose: pauseWithdrawlProcess,
+    isOpen: showingWithdrawlRequestOutcome,
+    onOpen: showWithdrawlRequestOutcome,
+    onOpenChange: displayWithdrawlRequestOutcome,
+    onClose: hideWithdrawlRequestOutcome,
   } = useDisclosure();
 
   const [amountToBeWithdrawn, setAmountToBeWithdrawn] = useState(0);
-  const [withdrawProcessStep, setWithdrawlProcessStep] = useState(1);
 
   return (
     <div className="pb-20">
       <header className="py-5 md:pt-0 lg:pt-1 px-4 mb-2">
-        {/* <h1 className="text-2xl lg:text-3xl">Withdrawal Funds</h1> */}
-        <PWAPageTitle title="Withdraw Funds" />
+        <PWAPageTitle title="Withdrawl" />
       </header>
 
       <section className="pt-0 pb-4 overflow-y-auto px-4 max-w-xl">
-        <div
-          className={clsx({
-            "space-y-6 block pt-4": withdrawProcessStep === 1,
-            hidden: withdrawProcessStep !== 1,
-          })}
-        >
+        <div className={clsx("space-y-6 block pt-4")}>
           <div>
-            <h4 className="mb-2 text-sm text-zinc-100">Receiving Account</h4>
-            <article className="flex gap-x-4 items-center p-2.5 bg-zinc-800 rounded-xl pe-4">
+            <h4 className="mb-3 text-sm text-zinc-100">Receiving Account</h4>
+            <article className="flex gap-x-4 items-center p-2 bg-default-100 rounded-xl">
               <div className="p-4 rounded-lg text-primary bg-primary-50 shadow">
                 <LandmarkIcon className="size-7" />
               </div>
-              <div className="flex-grow w-full text-sm text-zinc-300">
+              <div className="flex-grow w-full text-sm text-zinc-200">
                 <p>Palmpay</p>
                 <p>999 282 0202</p>
                 <p>Sunday Awanu Paul</p>
               </div>
-              <Button variant="light" isIconOnly size="md" radius="full">
+              <Button variant="light" isIconOnly size="lg" radius="full">
                 <PenBoxIcon className="size-5 text-zinc-200" />
               </Button>
             </article>
@@ -75,7 +78,7 @@ export default function WithdrawlPage() {
             />
 
             <p className="text-sm text-zinc-400 flex items-center gap-x-1.5 py-2.5 px-1.5">
-              <WalletIcon className="size-4" /> Balance: #18,000.00
+              <WalletIcon className="size-4" /> Balance: 18,000.00
             </p>
           </div>
 
@@ -85,92 +88,91 @@ export default function WithdrawlPage() {
                 <Button
                   key={`${amount}-${index}`}
                   radius="full"
-                  size="md"
-                  className="h-9"
+                  size="lg"
+                  className="h-10 min-w-0 px-3"
                 >
-                  # {amount}
+                  #{amount}
                 </Button>
               );
             })}
           </div>
 
-          <footer className="pt-4">
-            <div className="lg:hidden mb-2">
-              <div className="grid grid-cols-3 gap-1.5">
-                {[1, 2, 3, 4, 5, 9, 0, "00"].map((digit, index) => {
-                  return (
-                    <Button
-                      key={`${digit}#${index}`}
-                      radius="sm"
-                      className="text-xl font-medium"
-                    >
-                      {digit}
-                    </Button>
-                  );
-                })}
-                <Button radius="sm" className="text-xl font-medium">
-                  <DeleteIcon />
-                </Button>
-              </div>
-            </div>
-            <Button
-              variant="solid"
-              color="primary"
-              size="lg"
-              radius="md"
-              fullWidth
-              onClick={() => setWithdrawlProcessStep(2)}
-              className="font-semibold"
-              // endContent={
-              //   <span className="ms-auto">
-              //     <ChevronsRightIcon className="size-5" />
-              //   </span>
-              // }
-              isDisabled={!amountToBeWithdrawn}
-            >
-              Withdrawl
-            </Button>
-          </footer>
+          <Button
+            variant="solid"
+            color="primary"
+            size="lg"
+            radius="md"
+            fullWidth
+            onClick={() => showWithdrawlRequestOutcome()}
+            className="font-semibold"
+            isDisabled={!amountToBeWithdrawn}
+          >
+            Withdraw
+          </Button>
         </div>
 
-        <div
-          className={clsx("space-y-6", {
-            block: withdrawProcessStep === 2,
-            hidden: withdrawProcessStep !== 2,
-          })}
+        <Modal
+          isOpen={showingWithdrawlRequestOutcome}
+          onOpenChange={displayWithdrawlRequestOutcome}
+          backdrop="opaque"
+          placement="center"
+          hideCloseButton
+          isDismissable={false}
         >
-          <div className="text-center flex flex-col items-center gap-y-4 py-4">
-            <CheckCircleIcon className="text-success size-16" />
-            <p className="text-lg">Withdrawl Request Successful!</p>
-          </div>
-
-          <footer className="pt-6 flex items-center gap-x-2">
-            <Button
-              variant="flat"
-              color="success"
-              size="lg"
-              radius="md"
-              fullWidth
-              onClick={() => {
-                setWithdrawlProcessStep(1);
-              }}
-            >
-              Withdrawl Again!
-            </Button>
-            <Button
-              as={Link}
-              href="/transactions/#8482"
-              variant="flat"
-              color="warning"
-              size="lg"
-              radius="md"
-              fullWidth
-              onClick={() => pauseWithdrawlProcess()}
-            >
-              View Status
-            </Button>
-          </footer>
-        </div>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalBody className="px-6 py-12 flex items-center justify-center">
+                  <ClipboardSignature className="size-10 text-warning mb-4" />
+                  <h3 className="text-xl">
+                    Your transaction is being reviewed
+                  </h3>
+                  <p className="text-zinc-400 text-xs flex gap-x-2 items-center">
+                    <TimerIcon className="size-4" /> Average wait time: 2 min
+                  </p>
+                </ModalBody>
+                <ModalFooter className="flex-wrap gap-4">
+                  <Button
+                    as={Link}
+                    size="lg"
+                    href="/transactions"
+                    color="primary"
+                    variant="solid"
+                    onPress={onClose}
+                    className="grow"
+                    radius="sm"
+                  >
+                    Withdrawl again
+                  </Button>
+                  <Button
+                    as={Link}
+                    size="lg"
+                    href="/transactions"
+                    color="primary"
+                    variant="solid"
+                    onPress={onClose}
+                    className="grow"
+                    radius="sm"
+                  >
+                    View status
+                  </Button>
+                  <Button
+                    as={Link}
+                    href="/dashboard"
+                    size="lg"
+                    color="primary"
+                    variant="flat"
+                    onPress={onClose}
+                    className="w-full"
+                    radius="sm"
+                  >
+                    Back to Dashboard
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </section>
     </div>
   );
