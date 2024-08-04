5234b82b95b96c446f368a9d63b6481e46d34449
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-08-03 17:08:51 -0700

    feat: add select filter for funds flow

---
+++ app/(pwa)/transactions/funds-flow-select.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Select, SelectItem, Selection } from "@nextui-org/react";
+
+const flows = [
+  {
+    key: "out-flow",
+    label: "Out",
+  },
+  {
+    key: "in-flow",
+    label: "In",
+  },
+];
+
+export default function FundsFlowSelect() {
+  return (
+    <div className="flex w-full max-w-xs flex-col gap-2">
+      <Select
+        label="Money Flow"
+        radius="md"
+        defaultSelectedKeys={["out-flow"]}
+        className="max-w-[45%]"
+      >
+        {flows.map((flow) => (
+          <SelectItem key={flow.key}>{flow.label}</SelectItem>
+        ))}
+      </Select>
+    </div>
+  );
+}
