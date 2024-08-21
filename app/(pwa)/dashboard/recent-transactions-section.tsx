a7e1d5e1ae0234363fec7ddf926506e4d3410951
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-08-21 13:02:50 -0700

    fix: replace with standards and existing designs

--- app/(pwa)/dashboard/recent-transactions-section.tsx
+++ app/(pwa)/dashboard/recent-transactions-section.tsx
@@ -2,66 +2,57 @@ import { BadgeDollarSign, HandshakeIcon, Landmark } from "lucide-react";
 import { Button } from "@nextui-org/react";
 import icons from "currency-icons";
 import Link from "next/link";
+import PWASectionTitle from "../section-title";
 
 export default function RecentTransactionsSection() {
   return (
     <section className="w-full">
       <header className="mb-4 flex flex-wrap lg:flex-nowrap items-center justify-between gap-x-8 gap-y-1">
-        <h2 className="text-medium font-medium whitespace-nowrap">
-          Recent Transactions
-        </h2>
+        <PWASectionTitle title="Recent Transactions" />
         <Button
           variant="light"
           size="sm"
           color="primary"
-          className="p-0 underline text-sm underline-offset-4"
+          className="p-0 text-sm"
           href="/transactions"
           as={Link}
         >
           View all
         </Button>
       </header>
-      <article className="flex flex-col gap-4">
+      <article className="flex flex-col gap-y-5">
         <div className="flex items-center justify-between gap-4">
           <div className="flex items-center gap-3">
             <div className="flex items-center justify-center rounded-md p-3 bg-success-50 text-success">
-              <HandshakeIcon className="size-6" />
+              <HandshakeIcon className="size-5" />
             </div>
             <div>
-              <p className="text-sm font-meidum mb-1">
-                Investment
-              </p>
-              <p className="text-xs text-zinc-400">
-                May 20th at 9:00pm
-              </p>
+              <p className="text-sm font-meidum mb-1">Investment</p>
+              <p className="text-xs text-zinc-400">May 20th at 9:00pm</p>
             </div>
           </div>
           <div className="grid justify-items-end">
-            <p className="text-sm lg:text-base whitespace-nowrap text-success">
-              + {icons["NGN"]?.symbol || "₿"} 0.0005
+            <p className="text-medium lg:text-base whitespace-nowrap">
+              + {icons["NGN"]?.symbol || "₿"}0.0005
             </p>
-            {/* <p className="text-sm text-success font-semibold">Successful</p> */}
+            <p className="text-xs text-success">Successful</p>
           </div>
         </div>
         <div className="flex items-center justify-between gap-4">
           <div className="flex items-center gap-3">
             <div className="flex items-center justify-center rounded-md p-3 bg-secondary-50 text-secondary">
-              <Landmark className="size-6" />
+              <Landmark className="size-5" />
             </div>
             <div>
-              <p className="text-sm font-meidum mb-1">
-                Withdrawal
-              </p>
-              <p className="text-xs text-zinc-400">
-                May 20th at 05:50am
-              </p>
+              <p className="text-sm font-meidum mb-1">Withdrawal</p>
+              <p className="text-xs text-zinc-400">May 20th at 05:50am</p>
             </div>
           </div>
           <div className="grid justify-items-end">
-            <p className="text-sm lg:text-base whitespace-nowrap text-danger">
-              - {icons["NGN"]?.symbol || "#"} 10,000.00
+            <p className="text-medium lg:text-base whitespace-nowrap">
+              - {icons["NGN"]?.symbol || "#"}10,000.00
             </p>
-            {/* <p className="text-sm text-danger font-semibold">Failed</p> */}
+            <p className="text-xs text-danger">Failed</p>
           </div>
         </div>
       </article>
