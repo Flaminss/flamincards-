9392d0812244b00dcc114def0a862a0c94b16ae7
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-08-21 13:01:14 -0700

    feat: add page base layout structure

--- app/(pwa)/crypto/page.tsx
+++ app/(pwa)/crypto/page.tsx
@@ -4,16 +4,24 @@
 import CryptoSalePitchStep from "./pitch-step";
 import CryptoSalePaymentStep from "./payment-step";
 import { Button } from "@nextui-org/button";
+import PWAPageTitle from "../page-title";
 
 export default function CryptoSellPage() {
   return (
-    <>
+    <div className="pb-20 max-w-xl lg:max-w-[unset] mx-auto">
+      <header className="py-5 md:pt-0 lg:pt-1 px-4 mb-2">
+        <PWAPageTitle title="Web3 Marketplace" />
+      </header>
+
       {/* <MultiStep
         prevButton={{ title: "Go Back", style: { paddingBlock: "2rem" } }}
       >
       </MultiStep> */}
-      <CryptoSalePitchStep title="Provide Information" />
-      <CryptoSalePaymentStep title="Payment Procedure" />
-    </>
+
+      <div className="px-4">
+        <CryptoSalePitchStep title="Provide Information" />
+        {/* <CryptoSalePaymentStep title="Payment Procedure" /> */}
+      </div>
+    </div>
   );
 }
