5e7ca734f8be663a2486d88157d9b79fcf37f1d2
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-18 04:00:21 -0700

    feat: add profile page with extracted form for edit

---
+++ app/(pwa)/account/profile/page.tsx
@@ -0,0 +1,45 @@
+import { Link } from "@mui/material";
+import { Button, DatePicker, Input } from "@nextui-org/react";
+import clsx from "clsx";
+import TransactionPINInput from "../transaction-pin-input";
+
+export function ProfilePage() {
+  return (
+    <form className={clsx("pt-10")}>
+      <section className="grid gap-y-6 mb-10">
+        <div className="flex flex-wrap sm:flex-nowrap gap-4 w-full">
+          <Input size="md" radius="sm" label="First Name" />
+          <Input size="md" radius="sm" label="Last Name" />
+        </div>
+        <DatePicker
+          isRequired
+          radius="sm"
+          label="Date of Birth"
+          showMonthAndYearPickers
+        />
+        {/* <Input size="md" radius="sm" label="Transaction PIN" /> */}
+        <div className="bg-zinc-800 rounded-lg p-2.5">
+          <label
+            htmlFor="transaction-pin"
+            className="text-sm text-zinc-300 block mb-1.5"
+          >
+            Transaction PIN
+          </label>
+          <TransactionPINInput />
+        </div>
+      </section>
+
+      <Button
+        as={Link}
+        href="/dashboard"
+        color="primary"
+        variant="solid"
+        fullWidth
+        radius="sm"
+        size="lg"
+      >
+        Done! Go-to Dashboard
+      </Button>
+    </form>
+  );
+}
