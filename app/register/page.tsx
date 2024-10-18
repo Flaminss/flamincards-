fbc0f96b285ea32e42cb3302f58c5276b52acb32
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-18 04:16:28 -0700

    feat: prevent logged in user from registering as new customer

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Input, Link, Spinner } from "@nextui-org/react";
 import { MailPlus, XIcon } from "lucide-react";
 import { useUserAuthContext } from "../user-auth-provider";
@@ -98,6 +98,12 @@ export default function RegisterPage() {
     },
   };
 
+  useEffect(() => {
+    if (userAuthContext.current) {
+      router.replace("/account");
+    }
+  });
+
   return (
     <div className="min-h-screen flex flex-col bg-black">
       <AuthFlowNavigationTop
@@ -108,102 +114,104 @@ export default function RegisterPage() {
         )}
       />
 
-      <main className="pt-12 py-8 px-4 max-w-lg mx-auto">
-        <header>
-          <MailPlus className="block mx-auto size-10 mb-2 sm:size-12 sm:mb-4" />
-          <h1 className="text-2xl sm:text-3xl text-center mb-1 sm:mb-2 font-medium">
-            Create an account ✨
-          </h1>
-          <p className="text-sm text-zinc-400 text-center">
-            Start your exciting journey with us today.
-          </p>
-        </header>
+      <main>
+        <section className="pt-12 py-8 px-4 max-w-lg mx-auto">
+          <header>
+            <MailPlus className="block mx-auto size-10 mb-2 sm:size-12 sm:mb-4" />
+            <h1 className="text-2xl sm:text-3xl text-center mb-1 sm:mb-2 font-medium">
+              Create an account ✨
+            </h1>
+            <p className="text-sm text-zinc-400 text-center">
+              Start your exciting journey with us today.
+            </p>
+          </header>
 
-        <form className={clsx("pt-10")}>
-          <p
-            data-description="general-form-error"
-            className={clsx(
-              " flex items-center gap-x-6 p-2.5 mb-10 border border-danger-300 bg-danger-50 rounded-lg text-sm text-danger-900",
-              {
-                hidden: formErrors._all === null,
-              }
-            )}
-          >
-            <span className="w-full px-1">{formErrors?._all}</span>
+          <form className={clsx("pt-10")}>
+            <p
+              data-description="general-form-error"
+              className={clsx(
+                " flex items-center gap-x-6 p-2.5 mb-10 border border-danger-300 bg-danger-50 rounded-lg text-sm text-danger-900",
+                {
+                  hidden: formErrors._all === null,
+                }
+              )}
+            >
+              <span className="w-full px-1">{formErrors?._all}</span>
+
+              <Button
+                isIconOnly
+                size="sm"
+                color="danger"
+                radius="sm"
+                onPress={clearGeneralFormError}
+              >
+                <XIcon className="size-4" />
+              </Button>
+            </p>
+
+            <section className="grid gap-y-6 mb-10">
+              <Input
+                isRequired
+                size="md"
+                type="email"
+                radius="sm"
+                label="Email"
+                placeholder=""
+                value={email}
+                onValueChange={setEmail}
+                isInvalid={formErrors?.email !== null}
+                errorMessage={formErrors?.email}
+                classNames={{ errorMessage: inputErrorClassNames }}
+              />
+
+              <Input
+                radius="sm"
+                type="password"
+                label="Password"
+                value={password}
+                onValueChange={setPassword}
+                isInvalid={formErrors?.password !== null}
+                errorMessage={
+                  formErrors?.password && formErrors?.password.length === 1 ? (
+                    formErrors?.password
+                  ) : (
+                    <ul className="flex flex-col gap-y-1 list-disc marker:text-danger-400 ps-5">
+                      {formErrors?.password?.map((error, index) => {
+                        return <li key={index}>{error}</li>;
+                      })}
+                    </ul>
+                  )
+                }
+                classNames={{ errorMessage: inputErrorClassNames }}
+              />
+
+              <Input
+                radius="sm"
+                type="password"
+                label="Re-type Password"
+                value={retypedPassword}
+                onValueChange={setRetypedPassword}
+                isClearable
+                isInvalid={formErrors?.retypedPassword !== null}
+                errorMessage={formErrors?.retypedPassword}
+                classNames={{ errorMessage: inputErrorClassNames }}
+              />
+            </section>
 
             <Button
-              isIconOnly
-              size="sm"
-              color="danger"
+              color={registerButtonStyle.color() as any}
+              variant="solid"
+              fullWidth
+              isDisabled={registrationProgress === "done"}
               radius="sm"
-              onPress={clearGeneralFormError}
+              size="lg"
+              onPress={() => handleRegisteration()}
             >
-              <XIcon className="size-4" />
+              {registrationProgress !== "processing" && "Register"}
+              {registrationProgress === "processing" && <Spinner size="sm" />}
             </Button>
-          </p>
-
-          <section className="grid gap-y-6 mb-10">
-            <Input
-              isRequired
-              size="md"
-              type="email"
-              radius="sm"
-              label="Email"
-              placeholder=""
-              value={email}
-              onValueChange={setEmail}
-              isInvalid={formErrors?.email !== null}
-              errorMessage={formErrors?.email}
-              classNames={{ errorMessage: inputErrorClassNames }}
-            />
-
-            <Input
-              radius="sm"
-              type="password"
-              label="Password"
-              value={password}
-              onValueChange={setPassword}
-              isInvalid={formErrors?.password !== null}
-              errorMessage={
-                formErrors?.password && formErrors?.password.length === 1 ? (
-                  formErrors?.password
-                ) : (
-                  <ul className="flex flex-col gap-y-1 list-disc marker:text-danger-400 ps-5">
-                    {formErrors?.password?.map((error, index) => {
-                      return <li key={index}>{error}</li>;
-                    })}
-                  </ul>
-                )
-              }
-              classNames={{ errorMessage: inputErrorClassNames }}
-            />
-
-            <Input
-              radius="sm"
-              type="password"
-              label="Re-type Password"
-              value={retypedPassword}
-              onValueChange={setRetypedPassword}
-              isClearable
-              isInvalid={formErrors?.retypedPassword !== null}
-              errorMessage={formErrors?.retypedPassword}
-              classNames={{ errorMessage: inputErrorClassNames }}
-            />
-          </section>
-
-          <Button
-            color={registerButtonStyle.color() as any}
-            variant="solid"
-            fullWidth
-            isDisabled={registrationProgress === "done"}
-            radius="sm"
-            size="lg"
-            onPress={() => handleRegisteration()}
-          >
-            {registrationProgress !== "processing" && "Register"}
-            {registrationProgress === "processing" && <Spinner size="sm" />}
-          </Button>
-        </form>
+          </form>
+        </section>
       </main>
     </div>
   );
