b683ae954b2e5fd29386cfc832f2e4a3a8f116ca
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-19 11:21:29 -0700

    feat: replace placeholder text with practical production message

--- app/(pwa)/dashboard/annoucement-section.tsx
+++ app/(pwa)/dashboard/annoucement-section.tsx
@@ -7,16 +7,21 @@ import clsx from "clsx";
 
 // TODO: utilize "useToggle" package
 
+const message =
+  "🌈 New features are coming soon to enhance your shopping experience! Stay tuned!";
+
 export default function AnnoucementSection() {
   const [dismissed, setDismissed] = useState(false);
 
   const dismiss = () => setDismissed(true);
 
   return (
-    <section className={clsx("px-4 pb-8", { block: !dismissed, hidden: dismissed })}>
+    <section
+      className={clsx("px-4 pb-8", { block: !dismissed, hidden: dismissed })}
+    >
       <div className="shadow border slick-dark-bg rounded-lg ps-4 pe-2.5 sm:ps-5 py-3 flex gap-x-2.5 items-center">
         <MessageCircleWarningIcon className="size-6 shrink-0 text-primary me-1" />
-        <p className="w-full text-sm">We just launched some cool stuff!</p>
+        <p className="w-full text-sm">{message}</p>
         <Button
           isIconOnly
           size="sm"
