9ec478874503a865f4edb81c4ecd1beb0dc0a408
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-19 08:27:12 -0700

    fix: incorrect creaction of page file by framework conventions

--- app/(pwa)/account/profile/page.tsx
+++ app/(pwa)/account/profile/page.tsx
@@ -5,41 +5,43 @@ import TransactionPINInput from "../transaction-pin-input";
 
 export default function ProfilePage() {
   return (
-    <form className={clsx("pt-10")}>
-      <section className="grid gap-y-6 mb-10">
-        <div className="flex flex-wrap sm:flex-nowrap gap-4 w-full">
-          <Input size="md" radius="sm" label="First Name" />
-          <Input size="md" radius="sm" label="Last Name" />
-        </div>
-        <DatePicker
-          isRequired
-          radius="sm"
-          label="Date of Birth"
-          showMonthAndYearPickers
-        />
-        {/* <Input size="md" radius="sm" label="Transaction PIN" /> */}
-        <div className="bg-zinc-800 rounded-lg p-2.5">
-          <label
-            htmlFor="transaction-pin"
-            className="text-sm text-zinc-300 block mb-1.5"
-          >
-            Transaction PIN
-          </label>
-          <TransactionPINInput />
-        </div>
-      </section>
+    <main>
+      <form className={clsx("pt-10")}>
+        <section className="grid gap-y-6 mb-10">
+          <div className="flex flex-wrap sm:flex-nowrap gap-4 w-full">
+            <Input size="md" radius="sm" label="First Name" />
+            <Input size="md" radius="sm" label="Last Name" />
+          </div>
+          <DatePicker
+            isRequired
+            radius="sm"
+            label="Date of Birth"
+            showMonthAndYearPickers
+          />
+          {/* <Input size="md" radius="sm" label="Transaction PIN" /> */}
+          <div className="bg-zinc-800 rounded-lg p-2.5">
+            <label
+              htmlFor="transaction-pin"
+              className="text-sm text-zinc-300 block mb-1.5"
+            >
+              Transaction PIN
+            </label>
+            <TransactionPINInput />
+          </div>
+        </section>
 
-      <Button
-        as={Link}
-        href="/dashboard"
-        color="primary"
-        variant="solid"
-        fullWidth
-        radius="sm"
-        size="lg"
-      >
-        Done! Go-to Dashboard
-      </Button>
-    </form>
+        <Button
+          as={Link}
+          href="/dashboard"
+          color="primary"
+          variant="solid"
+          fullWidth
+          radius="sm"
+          size="lg"
+        >
+          Done! Go-to Dashboard
+        </Button>
+      </form>
+    </main>
   );
 }
