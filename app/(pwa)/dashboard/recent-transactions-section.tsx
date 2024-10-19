fa95bffa28cbda76e678c18e7a35431dd455cb3d
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-19 14:48:35 -0700

    feat: display ui for empty data collection

--- app/(pwa)/dashboard/recent-transactions-section.tsx
+++ app/(pwa)/dashboard/recent-transactions-section.tsx
@@ -1,13 +1,43 @@
-import { BadgeDollarSign, HandshakeIcon, Landmark } from "lucide-react";
-import { Button } from "@nextui-org/react";
+"use client";
+
+import { useState } from "react";
+import {
+  BadgeDollarSign,
+  HandshakeIcon,
+  Landmark,
+  ListIcon,
+  ListOrderedIcon,
+  ListTreeIcon,
+  ScrollIcon,
+} from "lucide-react";
+import { Button, Link } from "@nextui-org/react";
 import icons from "currency-icons";
-import Link from "next/link";
 import PWASectionTitle from "../section-title";
 import RecentTransactionItem, {
   TransactionType,
 } from "./recent-transactions-item";
 
 export default function RecentTransactionsSection() {
+  const transactions = [] as any[];
+  // const [transactions, setTransactions] = useState([
+  //   {
+  //     id: "293ko3uo2kj3",
+  //     type: "Investment" as TransactionType,
+  //     date: new Date(),
+  //     amount: 500,
+  //     flow: "in" as const,
+  //     status: "successful",
+  //   },
+  //   {
+  //     id: "293ko3uo2kj32e",
+  //     type: "Withdrawl" as TransactionType,
+  //     date: new Date(),
+  //     amount: 2000,
+  //     flow: "out" as const,
+  //     status: "successful",
+  //   },
+  // ]);
+
   return (
     <section className="w-full">
       <header className="mb-4 flex flex-wrap lg:flex-nowrap items-center justify-between gap-x-8 gap-y-1">
@@ -23,28 +53,28 @@ export default function RecentTransactionsSection() {
           View all
         </Button>
       </header>
-      <article className="flex flex-col gap-y-5">
-        {[
-          {
-            id: "293ko3uo2kj3",
-            type: "Investment" as TransactionType,
-            date: new Date(),
-            amount: 500,
-            flow: "in" as const,
-            status: "successful",
-          },
-          {
-            id: "293ko3uo2kj32e",
-            type: "Withdrawl" as TransactionType,
-            date: new Date(),
-            amount: 2000,
-            flow: "out" as const,
-            status: "successful",
-          },
-        ].map((transaction, index) => {
-          return <RecentTransactionItem key={index+transaction.id} {...transaction} />;
-        })}
-      </article>
+      {transactions.length === 0 ? (
+        <div className="rounded-md shadow bg-content1 grid gap-y-2.5 text-center place-items-center p-4 py-8">
+          <ListIcon className="size-8 mx-auto text-gray-600" />
+          <p className="text-sm text-gray-500 max-w-[32ch] ">
+            You have an empty transaction history. Redeem your{" "}
+            <Link size="sm" href="/giftcards" underline="always">
+              giftcards
+            </Link>
+          </p>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-y-5">
+          {transactions.map((transaction, index) => {
+            return (
+              <RecentTransactionItem
+                key={index + transaction.id}
+                {...transaction}
+              />
+            );
+          })}
+        </div>
+      )}
     </section>
   );
 }
