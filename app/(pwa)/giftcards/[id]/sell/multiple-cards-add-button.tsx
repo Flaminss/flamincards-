dd89017377a865c1b2166492d9c85149b75207ae
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-22 06:05:52 -0700

    feat: improve multiple card adding UI component aesthetics

--- app/(pwa)/giftcards/[id]/sell/multiple-cards-add-button.tsx
+++ app/(pwa)/giftcards/[id]/sell/multiple-cards-add-button.tsx
@@ -32,62 +32,66 @@ export default function MultipleCardAddButton() {
       <Modal
         isOpen={isOpen}
         onOpenChange={onOpenChange}
-        placement="bottom-center" 
+        placement="bottom-center"
         size="sm"
-        hideCloseButton
         scrollBehavior="inside"
-        className="z-50"
+        className="mb-16"
       >
         <ModalContent>
           {(onClose) => (
             <>
-              {/* <ModalHeader className="flex flex-col gap-1">
-              </ModalHeader> */}
-              <ModalBody>
-                <div className="grid gap-y-4">
-                  <Select
-                    label="Amount (#)"
-                    radius="md"
-                    size="sm"
-                    selectionMode="single"
-                    defaultSelectedKeys={[20]}
-                    selectorIcon={<ArrowDownCircleIcon />}
-                    classNames={{
-                      selectorIcon:
-                        "shrink-0 w-auto h-auto !size-4 text-zinc-400",
-                    }}
-                  >
-                    {[
-                      { label: "20", value: 20 },
-                      { label: "25", value: 25 },
-                      { label: "50", value: 50 },
-                    ].map((item) => (
-                      <SelectItem
-                        key={item.value}
-                        value={item.value}
-                        className="text-white"
-                      >
-                        {item.label}
-                      </SelectItem>
-                    ))}
-                  </Select>
+              <ModalHeader className="flex flex-col gap-1 pt-6 pb-4 px-4">
+                <h5 className="font-medium">Amazon (USA) Gift Card</h5>
+                <p className="text-xs text-gray-400">
+                  You're adding one of many cards to redeem
+                </p>
+              </ModalHeader>
+              <ModalBody className="grid gap-y-4 px-3">
+                <Select
+                  label="Amount (#)"
+                  radius="md"
+                  size="sm"
+                  selectionMode="single"
+                  defaultSelectedKeys={[20]}
+                  selectorIcon={<ArrowDownCircleIcon />}
+                  classNames={{
+                    selectorIcon:
+                      "shrink-0 w-auto h-auto !size-4 text-zinc-400",
+                  }}
+                >
+                  {[
+                    { label: "20", value: 20 },
+                    { label: "25", value: 25 },
+                    { label: "50", value: 50 },
+                  ].map((item) => (
+                    <SelectItem
+                      key={item.value}
+                      value={item.value}
+                      className="text-white"
+                    >
+                      {item.label}
+                    </SelectItem>
+                  ))}
+                </Select>
 
-                  <Input
-                    label="E-Code (Optional)"
-                    placeholder="XXXX-XXXX-XXXX"
-                    size="lg"
-                    radius="sm"
-                  />
+                <Input
+                  label="E-Code (Optional)"
+                  placeholder="XXXX-XXXX-XXXX"
+                  size="lg"
+                  radius="sm"
+                />
 
-                  <ProofInput />
-                </div>
+                <ProofInput />
               </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Done
+              <ModalFooter className="px-3 py-2">
+                <Button
+                  color="primary"
+                  radius="sm"
+                  size="lg"
+                  fullWidth
+                  onPress={onClose}
+                >
+                  Add Card
                 </Button>
               </ModalFooter>
             </>
