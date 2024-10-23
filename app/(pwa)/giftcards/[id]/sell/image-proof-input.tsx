8549391134356a5ac92c4c206555c28f3a1f4253
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-23 02:53:27 -0700

    feat: convert to controlled component

--- app/(pwa)/giftcards/[id]/sell/image-proof-input.tsx
+++ app/(pwa)/giftcards/[id]/sell/image-proof-input.tsx
@@ -1,9 +1,6 @@
 import { Button, Chip } from "@nextui-org/react";
 import { ImagePlusIcon, Trash2 } from "lucide-react";
-import { useState } from "react";
 import Files from "react-files";
-
-const maxUploadableFiles = 1;
 
 type FileInputPayload = {
   id: string;
@@ -25,44 +22,52 @@ type ImageProofInputProps = {
   onRemovePayload: (payloadId: string) => void;
 };
 
-export default function ImageProofInput() {
-  const [fileUploadError, setFileUplaodError] = useState("");
-  const [files, setFiles] = useState([] as FileInputPayload[]);
+export default function ImageProofInput({
+  payloads,
+  payloadUploadErrors,
+  payloadMaxByteSize,
+  allowUploadOnClick,
+  bulkSelectUploadMaxCount,
+  onChange,
+  onRemovePayload,
+}: ImageProofInputProps) {
+  // const [payloadUploadErrors, setFileUplaodError] = useState("");
+  // const [files, setFiles] = useState([] as FileInputPayload[]);
 
-  const handleChange = (newFiles: any[]) => {
-    setFiles((prevFiles) => {
-      let exceededLimit = false;
-      const curatedFiles = [...prevFiles];
+  // const handleChange = (newFiles: any[]) => {
+  //   setFiles((prevFiles) => {
+  //     let exceededLimit = false;
+  //     const curatedFiles = [...prevFiles];
 
-      newFiles.forEach((newFile) => {
-        if (curatedFiles.length < maxUploadableFiles) {
-          curatedFiles.push(newFile);
-        } else {
-          exceededLimit = true;
-        }
-      });
+  //     newFiles.forEach((newFile) => {
+  //       if (curatedFiles.length < maxUploadableFiles) {
+  //         curatedFiles.push(newFile);
+  //       } else {
+  //         exceededLimit = true;
+  //       }
+  //     });
 
-      setFileUplaodError(() => {
-        const message = "Max amount of payloads reached";
-        return !exceededLimit ? "" : message;
-      });
+  //     // setFileUplaodError(() => {
+  //       const message = "Max amount of payloads reached";
+  //       return !exceededLimit ? "" : message;
+  //     });
 
-      return curatedFiles;
-    });
-  };
+  //     return curatedFiles;
+  //   });
+  // };
 
-  const handleFileRemove = (fileId: any) => {
-    setFiles((prevFiles) => {
-      setFileUplaodError("");
-      return prevFiles.filter((prevFile) => prevFile.id !== fileId);
-    });
-  };
+  // const handleFileRemove = (fileId: any) => {
+  //   setFiles((prevFiles) => {
+  //     // setFileUplaodError("");
+  //     return prevFiles.filter((prevFile) => prevFile.id !== fileId);
+  //   });
+  // };
 
-  if (files.length > 0) {
+  if (payloads.length > 0) {
     return (
       <div className="relative border rounded-lg overflow-hidden">
         <img
-          src={files[0].preview.url}
+          src={payloads[0].preview.url}
           className="w-full min-h-20 object-cover max-h-[380px]"
         />
         <Button
@@ -74,7 +79,7 @@ export default function ImageProofInput() {
         >
           <Trash2
             className="size-5"
-            onClick={() => handleFileRemove(files[0].id)}
+            onClick={() => onRemovePayload(payloads[0].id)}
           />
         </Button>
       </div>
@@ -85,11 +90,11 @@ export default function ImageProofInput() {
     <Files
       className="flex flex-col items-center justify-center w-full h-48.. border.. border-dashed.. border-default-400 bg-default-100 rounded-md shadow cursor-pointer"
       accepts={["image/*", "video/mp4"]}
-      onChange={handleChange}
+      onChange={onChange}
       single
-      maxFiles={maxUploadableFiles}
-      maxFileSize={1_500_000}
-      clickable={!fileUploadError}
+      maxFiles={bulkSelectUploadMaxCount}
+      maxFileSize={payloadMaxByteSize}
+      clickable={allowUploadOnClick}
     >
       <div className="flex flex-col items-center justify-center text-center pt-8 pb-8 px-4">
         <ImagePlusIcon className="size-10 mb-4 text-zinc-400" />
