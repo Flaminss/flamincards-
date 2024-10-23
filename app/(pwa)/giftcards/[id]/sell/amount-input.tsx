ed53d4d3bf51f758ef75e2b5778a4a5834058a32
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-23 07:45:50 -0700

    feat: extract custom form input

---
+++ app/(pwa)/giftcards/[id]/sell/amount-input.tsx
@@ -0,0 +1,51 @@
+import { Input, Button } from "@nextui-org/react";
+
+type AmountInputProps = {
+  value: string;
+  onValueChange: (value: string) => void;
+};
+
+export default function AmountInput({
+  value,
+  onValueChange,
+}: AmountInputProps) {
+  return (
+    <Input
+      type="number"
+      label="Amount (#)"
+      radius="md"
+      size="lg"
+      value={value}
+      placeholder="50"
+      onValueChange={onValueChange}
+      classNames={{
+        input: "remove-browser-input-counter",
+      }}
+      endContent={
+        <div className="flex gap-x-2 items-center">
+          <Button
+            size="md"
+            radius="sm"
+            onClick={() => {
+              onValueChange(((parseInt(value) || 0) - 5).toString());
+            }}
+            className="px-2.5 min-w-[unset] text-base font-semibold items-center hover:border-2"
+          >
+            <span className="text-2xl text-warning leading-none">-</span>5
+          </Button>
+
+          <Button
+            size="md"
+            radius="sm"
+            onClick={() => {
+              onValueChange(((parseInt(value) || 0) + 5).toString());
+            }}
+            className="px-2.5 min-w-[unset] text-base font-semibold items-center hover:border-2"
+          >
+            <span className="text-2xl text-warning leading-none">+</span>5
+          </Button>
+        </div>
+      }
+    />
+  );
+}
