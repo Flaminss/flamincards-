6ec57eac3497899b74c59590615471bb22c9a61d
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-24 15:22:53 -0700

    feat: allow file based interaction with object

--- app/(pwa)/giftcards/[id]/sell/image-proof-input.tsx
+++ app/(pwa)/giftcards/[id]/sell/image-proof-input.tsx
@@ -11,7 +11,7 @@ export type FileInputPayload = {
   extension: string;
   url: string;
   sizeReadable: number;
-} & BlobPart;
+} & Blob;
 
 type ImageProofInputProps = {
   payloads: FileInputPayload[];
