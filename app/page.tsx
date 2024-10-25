bbc35445c12fa2423fed17b78ddaee6afbefb8b2
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-25 15:55:18 -0700

    feat: opmize page with server component implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,10 @@
-"use client";
-
-import {
-  Link,
-  Button,
-  Navbar,
-  NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
-  NavbarBrand,
-  NavbarItem,
-  NavbarMenuItem,
-} from "@nextui-org/react";
-import { useState } from "react";
-import { BrandName } from "./(appzone)/navigation-top";
-import { ChevronRightIcon } from "lucide-react";
+import NavigationBarTop from "@app/navigation-bar-top";
+import { Link, Button } from "@nextui-org/react";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col bg-black">
-      <TopNavigation />
+      <NavigationBarTop />
 
       <main className="flex-grow">
         <section id="hero" className="pt-20 md:pt-28 pb-20 px-4">
@@ -65,64 +51,3 @@ export default function HomePage() {
     </div>
   );
 }
-
-const TopNavigation = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const menuItems = [
-    { title: "Dashboard", href: "/dashboard" },
-    { title: "Blog", href: "gistnaija.com" },
-    { title: "FAQ", href: "/#faq" },
-    { title: "Contact us", href: "/" },
-    { title: "Sponsor us", href: "/sponsor" },
-  ];
-
-  return (
-    <Navbar onMenuOpenChange={setIsMenuOpen} classNames={{ wrapper: "px-4" }}>
-      <NavbarContent>
-        <NavbarBrand>
-          <BrandName className="sm:text-xl" />
-        </NavbarBrand>
-      </NavbarContent>
-
-      <NavbarContent className="hidden sm:flex gap-x-6" justify="end">
-        {menuItems.map(({ title, href }, id) => {
-          return (
-            <NavbarItem key={id}>
-              <Link
-                color="foreground"
-                href={href}
-                size="md"
-                className="px-2 py-2 font-medium"
-              >
-                {title}
-              </Link>
-            </NavbarItem>
-          );
-        })}
-      </NavbarContent>
-
-      <NavbarContent justify="end" className="sm:hidden">
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
-      </NavbarContent>
-
-      <NavbarMenu>
-        {menuItems.map(({ title, href }, id) => (
-          <NavbarMenuItem key={id}>
-            <Link
-              href={href}
-              color="foreground"
-              className="flex justify-between items-center gap-x-2 w-full py-2 font-medium"
-            >
-              {title}
-              <ChevronRightIcon className="" />
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
-  );
-};
