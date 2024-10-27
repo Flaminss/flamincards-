6336370ea10a28976531f5fc3153b17a09777985
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-27 05:32:44 -0700

    feat: add HTTP cycle handling abstraction for project

---
+++ lib/http-cycle-handler/index.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+// import logger from './logService';
+// import {
+//   toast
+// } from 'react-toastify';
+
+// axios.interceptors.response.use(null, (error) => {
+//   const expectedError =
+//     error.response &&
+//     error.response.status >= 400 &&
+//     error.response.status < 500;
+
+//     if (!expectedError) {
+//       logger.log(error);
+//     }
+
+//   return Promise.reject(error);
+// });
+
+export const handlerInstance = axios.create;
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  patch: axios.patch,
+  put: axios.put,
+  delete: axios.delete,
+};
+
+// MOVE TO DECLARATION FILE
+declare module "@lib/http-cycle-handler" {
+  interface HttpRequestConfig {
+    baseURL: string;
+    timeout: number;
+    headers: Record<string, string>;
+  }
+
+  interface HttpRequest {
+    handlerInstance(config: HttpRequestConfig): any;
+  }
+
+  const httpRequest: HttpRequest;
+}
