4d5b08a507d96a384f7883a8163d1cf3b519ab0c
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-10-28 05:29:15 -0700

    feat: add attribution link for development team

---
+++ app/developer-attribution-link.tsx
@@ -0,0 +1,15 @@
+import { Link } from "@nextui-org/react";
+
+export default function DeveloperAttributionLink() {
+  return (
+    <Link
+      isExternal
+      href="https://t.me/everurstruly"
+      title="Development team portfolio website"
+      className="text-default-600 font-mono font-medium flex items-center gap-x-2"
+    >
+      Developed by
+      <span className="text-primary font-semibold">YoursTruly</span>
+    </Link>
+  );
+}
