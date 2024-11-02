af5b9bf11ce4484945e562a4eac8397dc932353e
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-01 17:52:37 -0700

    feat: expose baas databse api

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -7,7 +7,38 @@ export const client = new Client()
   .setEndpoint(apiEndpoint)
   .setProject(projectId);
 
-export const account = new Account(client);
 export { ID } from "appwrite";
 
+export const account = new Account(client);
 export const database = new Databases(client);
+export const DATABSE_ID = "616f3b3b7f5b4";
+
+export const collections = {
+  USERS: {
+    id: "616f3b3b7f5b5",
+    BANK_BILLING: {
+      id: "616f3b3b7f5b5",
+    },
+  },
+  EXCHANGE_RATES: {
+    id: "616f3b3b7f5b5",
+  },
+  GIFTCARDS: {
+    id: "616f3b3b7f5b5",
+    REGION: {
+      id: "616f3b3b7f5b5",
+      VALUES: {
+        id: "616f3b3b7f5b5",
+      },
+    },
+    TRADE: {
+      id: "616f3b3b7f5b5",
+      VALUE: {
+        id: "616f3b3b7f5b5",
+      },
+    },
+  },
+  TRANSACTIONS: {
+    id: "616f3b3b7f5b5",
+  },
+};
