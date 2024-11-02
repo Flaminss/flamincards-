e3301d45053b2eebdac176efa97dcf2473ff3f90
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-01 17:49:48 -0700

    feat: add global auth state provider

---
+++ app/(auth)/user-auth-provider.tsx
@@ -0,0 +1,79 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { AppwriteException } from "appwrite";
+import loginUser, { AuthSession } from "@/modules/user/login";
+import logoutUser from "@/modules/user/logout";
+import registerUser from "@/modules/user/register";
+import getSession from "@/modules/user/get-session";
+
+type UserAuthCredentials = { email: string; password: string };
+
+interface UserAuthContext {
+  session: AuthSession | null;
+  login: (credentials: UserAuthCredentials) => Promise<void>;
+  logout: () => Promise<void>;
+  register: (credentials: UserAuthCredentials) => Promise<void>;
+}
+
+const UserAuthContext = createContext<UserAuthContext | undefined>(undefined);
+
+export function userUserAuthContext() {
+  const context = useContext(UserAuthContext);
+  if (!context) {
+    throw new Error(
+      "userUserAuthContext must be used within a UserAuthProvider"
+    );
+  }
+  return context;
+}
+
+interface UserAuthProvider {
+  children: ReactNode;
+}
+
+export function UserAuthProvider({ children }: UserAuthProvider) {
+  const [session, setSession] = useState<AuthSession | null>(null);
+
+  async function login(credentials: { email: string; password: string }) {
+    const session = await loginUser(credentials);
+    // const email = { address: credentials.email, verified: false };
+    setSession(session);
+  }
+
+  async function logout() {
+    setSession(null);
+    await logoutUser();
+  }
+
+  async function register(credentials: { email: string; password: string }) {
+    await registerUser(credentials);
+  }
+
+  async function loadSession() {
+    try {
+      const session = await getSession();
+      setSession(session);
+    } catch (exception) {
+      if (exception instanceof AppwriteException) {
+        setSession(null);
+      } else {
+        throw exception;
+      }
+    }
+  }
+
+  useEffect(() => {
+    loadSession();
+  }, []);
+
+  return (
+    <UserAuthContext.Provider value={{ session, login, logout, register }}>
+      {children}
+    </UserAuthContext.Provider>
+  );
+}
