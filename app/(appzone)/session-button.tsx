60225d4f86b298e77d91c65185c54eb237d2350a
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-01 17:44:29 -0700

    feat: add session status feedback ui

---
+++ app/(appzone)/session-button.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@nextui-org/react";
+import { LogInIcon, UserCircle2Icon } from "lucide-react";
+import { userUserAuthContext } from "@app/(auth)/user-auth-provider";
+import Link from "next/link";
+
+export default function SessionButton() {
+  const userAuth = userUserAuthContext();
+
+  if (userAuth.session) {
+    return (
+      <Button
+        isIconOnly
+        radius="lg"
+        aria-label="unread notifications count"
+        variant="flat"
+        size="md"
+        className="text-zinc-300"
+        href="/account"
+        as={Link}
+      >
+        <UserCircle2Icon className="size-6" />
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      radius="lg"
+      color="primary"
+      variant="flat"
+      size="md"
+      endContent={<LogInIcon className="size-5" />}
+    >
+      Login
+    </Button>
+  );
+}
