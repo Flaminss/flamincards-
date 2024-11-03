e8efc44c5e5244d1daf19359d4fbf8a17d4d0972
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-02 18:22:15 -0700

    fix: broken ids and provide unavailable apis

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,5 @@
-import { Client, Account, Databases } from "appwrite";
+export { ID } from "appwrite";
+import { Client, Account, Databases, Storage } from "appwrite";
 
 const projectId = "669a882300077e41dbec";
 const apiEndpoint = "https://cloud.appwrite.io/v1";
@@ -7,38 +8,38 @@ export const client = new Client()
   .setEndpoint(apiEndpoint)
   .setProject(projectId);
 
-export { ID } from "appwrite";
-
 export const account = new Account(client);
-export const database = new Databases(client);
-export const DATABSE_ID = "616f3b3b7f5b4";
 
+export const mediaStorage = new Storage(client);
+
+export const databases = new Databases(client);
+export const DATABSE_ID = "rmldb";
 export const collections = {
   USERS: {
-    id: "616f3b3b7f5b5",
+    id: null,
     BANK_BILLING: {
-      id: "616f3b3b7f5b5",
+      id: "67240c1e0000e0b98a46",
     },
   },
   EXCHANGE_RATES: {
-    id: "616f3b3b7f5b5",
+    id: "67058e57003a5b47a69d",
   },
   GIFTCARDS: {
-    id: "616f3b3b7f5b5",
+    id: "6702f585002e232c7bb9",
     REGION: {
-      id: "616f3b3b7f5b5",
+      id: "67058aea003289efb0f3",
       VALUES: {
-        id: "616f3b3b7f5b5",
+        id: "67058d040009cf264c23",
       },
     },
     TRADE: {
-      id: "616f3b3b7f5b5",
+      id: "6723fc860013466a0eda",
       VALUE: {
-        id: "616f3b3b7f5b5",
+        id: "6723fe0b0004be1dfdc1",
       },
     },
   },
   TRANSACTIONS: {
-    id: "616f3b3b7f5b5",
+    id: "6702f5d60009ef1321e9",
   },
 };
