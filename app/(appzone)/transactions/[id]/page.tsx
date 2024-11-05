06b802895b886202f4976dc38c23341fb21fcff1
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-05 14:39:22 -0800

    feat: redesign card actions and info

--- app/(appzone)/transactions/[id]/page.tsx
+++ app/(appzone)/transactions/[id]/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { useState } from "react";
 import {
-  Tabs,
-  Tab,
   Card,
   CardBody,
   Button,
@@ -16,7 +13,7 @@ import PWAPageTitle from "../../page-title";
 import { CheckCircle, Copy, Headphones, Landmark } from "lucide-react";
 
 export default function TransactionDetails() {
-  const [activeTab, setActiveTab] = useState("details");
+  const status = "pending";
 
   const transaction = {
     id: "TX123456",
@@ -45,7 +42,7 @@ export default function TransactionDetails() {
   };
 
   const handleTroubleshootClick = () => {
-    setActiveTab("troubleshooting");
+    // handle troubleshoot click
   };
 
   return (
@@ -54,19 +51,14 @@ export default function TransactionDetails() {
         <PWAPageTitle title="Transaction Detail" />
       </header>
 
-      <section className="px-4 py-6 max-w-xl">
-        <Card className="overflow-visible mb-8">
+      <section className="px-4 py-6 max-w-xl grid gap-y-4">
+        <Card className="overflow-visible">
           <CardBody className="items-center justify-items-center py-6 px-4 grid gap-y-6 overflow-visible">
             <div className="-mt-10">
-              <Chip
-                size="lg"
-                radius="md"
-                color="success"
-                className="mb-2 border shadow h-14"
-              >
-                <Landmark className="size-7" />
-              </Chip>
-              <h4 className="text-center text-sm sm:text-medium text-zinc-400">
+              <div className="shadow-lg mb-2 size-16 rounded-xl bg-green-950 grid place-content-center">
+                <Landmark className="size-8 text-green-100" />
+              </div>
+              <h4 className="text-center text-sm sm:text-medium font-medium text-green-100">
                 Withdrawl
               </h4>
             </div>
@@ -75,89 +67,105 @@ export default function TransactionDetails() {
               <h4 className="hidden text-sm sm:text-medium text-zinc-400">
                 Withdrawl
               </h4>
-              <h4 className="text-4xl lg:text-4xl">+ $2,000</h4>
+              <h4 className="text-3xl lg:text-4xl font-medium">$2,000</h4>
             </div>
-            <p className="flex text-nowrap gap-x-2 items-center text-zinc-400">
+            <p className="flex text-nowrap gap-x-2 items-center text-success-400">
               <CheckCircle className="text-success size-4" />
               Successful
             </p>
           </CardBody>
         </Card>
 
-        <Tabs
-          aria-label="Options"
-          classNames={{ base: "w-full", tabList: "w-full max-w-sm mx-auto" }}
-          size="lg"
-        >
-          <Tab key="details" title="Details" className="grid gap-y-6">
-            <Card>
-              <CardHeader>Summary</CardHeader>
-              <CardBody className="text-sm">
-                <ul className="grid gap-y-3">
-                  <li className="flex justify-between gap-x-6 items-center text-zinc-400">
-                    ID:{" "}
-                    <div className="flex gap-x-2">
-                      <span className="text-white">{transaction.id}</span>{" "}
-                      <Copy className="size-4 text-zinc-400" />
-                    </div>
-                  </li>
-                  <li className="flex justify-between gap-x-6 items-center text-zinc-400">
-                    Date: <span className="text-white">{transaction.date}</span>
-                  </li>
-                  <li className="flex justify-between gap-x-6 items-center text-zinc-400">
-                    Amount ($):
-                    <span className="text-white">
-                      {transaction.amount.toFixed(2)}
-                    </span>
-                  </li>
-                  <li className="flex justify-between gap-x-6 items-center text-zinc-400">
-                    Fees ($):
-                    <span className="text-white">
-                      {transaction.fees.toFixed(2)}
-                    </span>
-                  </li>
-                  <li className="flex justify-between gap-x-6 border-t border-dashed mt-4 pt-5 pb-2.5 items-center text-zinc-400">
-                    Total Amount: $
-                    <span className="text-white">
-                      {transaction.totalAmount.toFixed(2)}
-                    </span>
-                  </li>
-                </ul>
-              </CardBody>
-            </Card>
+        <Card>
+          <CardHeader>Summary</CardHeader>
+          <CardBody className="text-sm">
+            <ul className="grid gap-y-3">
+              <li className="flex justify-between gap-x-6 items-center text-zinc-400">
+                ID:{" "}
+                <div className="flex gap-x-2">
+                  <span className="text-white">{transaction.id}</span>{" "}
+                  <Copy className="size-4 text-zinc-400" />
+                </div>
+              </li>
+              <li className="flex justify-between gap-x-6 items-center text-zinc-400">
+                Date: <span className="text-white">{transaction.date}</span>
+              </li>
+              <li className="flex justify-between gap-x-6 items-center text-zinc-400">
+                Amount ($):
+                <span className="text-white">
+                  {transaction.amount.toFixed(2)}
+                </span>
+              </li>
+              <li className="flex justify-between gap-x-6 items-center text-zinc-400">
+                Fees ($):
+                <span className="text-white">
+                  {transaction.fees.toFixed(2)}
+                </span>
+              </li>
+              <li className="flex justify-between gap-x-6 border-t border-dashed mt-4 pt-5 pb-2.5 items-center text-zinc-400">
+                Total Amount: $
+                <span className="text-white">
+                  {transaction.totalAmount.toFixed(2)}
+                </span>
+              </li>
+            </ul>
+          </CardBody>
+        </Card>
 
-            <Card>
-              <CardBody className="text-center justify-center p-4">
-                <p className="text-zinc-300 mb-4 font-light">
-                  Any complaints or questions about this transaction?
-                </p>
-                <Button
-                  variant="flat"
-                  color="primary"
-                  radius="sm"
-                  onClick={handleTroubleshootClick}
-                  startContent={<Headphones className="size-4" />}
-                  className="font-medium w-fit mx-auto"
-                >
-                  Customer Support
-                </Button>
-              </CardBody>
-            </Card>
+        <Card>
+          <CardBody className="text-sm">
+            <div className="flex justify-between gap-x-6 items-center text-zinc-400">
+              <span>Sender:</span>
+              <span className="text-white">{transaction.sender.name}</span>
+            </div>
+            <div className="flex justify-between gap-x-6 items-center text-zinc-400">
+              <span>Account:</span>
+              <span className="text-white">{transaction.sender.account}</span>
+            </div>
+          </CardBody>
+        </Card>
 
-            <Button radius="sm" color="primary" variant="solid" size="lg">
+        <Card>
+          <CardBody className="text-center justify-center p-4">
+            <p className="text-zinc-300 mb-4 font-light">
+              Any complaints or questions about this transaction?
+            </p>
+            <Button
+              variant="flat"
+              color="primary"
+              radius="sm"
+              onClick={handleTroubleshootClick}
+              startContent={<Headphones className="size-4" />}
+              className="font-medium w-fit mx-auto"
+            >
+              Customer Support
+            </Button>
+          </CardBody>
+        </Card>
+
+        <div className="grid pt-6">
+          {status === "pending" ? (
+            <Button
+              radius="sm"
+              color="warning"
+              variant="flat"
+              size="lg"
+              fullWidth
+            >
+              Cancel Order
+            </Button>
+          ) : (
+            <Button
+              radius="sm"
+              color="primary"
+              variant="solid"
+              size="lg"
+              fullWidth
+            >
               Save Receipt
             </Button>
-          </Tab>
-          <Tab key="support" title="Customer Support">
-            <Card>
-              <CardHeader>Troubleshooting</CardHeader>
-              <CardBody>
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                qui officia deserunt mollit anim id est laborum.
-              </CardBody>
-            </Card>
-          </Tab>
-        </Tabs>
+          )}
+        </div>
       </section>
     </div>
   );
