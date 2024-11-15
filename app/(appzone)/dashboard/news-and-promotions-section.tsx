8c51b4e1e9787a66d77f29bdb817abaa4348905a
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-14 17:26:35 -0800

    feat: make cards and typography more responsive

--- app/(appzone)/dashboard/news-and-promotions-section.tsx
+++ app/(appzone)/dashboard/news-and-promotions-section.tsx
@@ -1,25 +1,34 @@
 import Link from "next/link";
 import { Card } from "@nextui-org/react";
 import PWASectionTitle from "../section-title";
+import clsx from "clsx";
 
 export default function NewsAndPromotionsSection() {
   return (
     <section className="px-4 py-8">
       <PWASectionTitle title="Latest News, Trends and Promotions" />
 
-      <Card about="carousel" className="h-[260px] md:h-[320px] w-full mt-4 mb-6" />
+      <Card
+        about="carousel"
+        className="h-[260px] md:h-[320px] w-full mt-4 mb-6"
+      />
 
       <div className="flex flex-wrap lg:flex-nowrap gap-8 lg:gap-10">
-        {[1, 2, 3].map((article) => {
+        {[1, 2, 3].map((article, index) => {
           return (
             <article
               key={article}
-              className="flex items-center gap-x-4 sm:max-w-[34ch] grow"
+              className={clsx(
+                "flex items-center gap-x-4 max-w-[38ch] sm:max-w-[52ch] grow"
+                // {
+                //   "ms-auto": (index + 1) % 2 === 0,
+                // }
+              )}
             >
               <figure className="w-16 h-12 bg-content1"></figure>
               <Link
                 href="/"
-                className="underline text-medium xl:text-base underline-offset-2"
+                className="underline text-medium sm:text-xl xl:text-base underline-offset-2"
               >
                 {article === 1
                   ? "Top 10 Nigerian Movies to Watch This Year"
