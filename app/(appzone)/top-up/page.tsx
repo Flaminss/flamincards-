21a9ba3f96cf67e120688d631f7c963dd31026a0
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-15 10:25:01 -0800

    feat: add new service rendering page

---
+++ app/(appzone)/top-up/page.tsx
@@ -0,0 +1,15 @@
+import PWAPageTitle from "../page-title";
+
+export default function TopUpPage() {
+  return (
+    <div className="pb-20 max-w-xl lg:max-w-[unset] mx-auto">
+      <header className="py-5 md:pt-0 lg:pt-1 px-4 mb-2">
+        <PWAPageTitle title="Dashboard" />
+      </header>
+
+      <section>
+        
+      </section>
+    </div>
+  );
+}
