5fd0fe13dd6229ad4860b197602489a7a528152e
Oghenetefa Okotete <oghenetefa@gmail.com>
2024-11-15 10:25:19 -0800

    fix: broken link

--- app/(appzone)/app-dock.tsx
+++ app/(appzone)/app-dock.tsx
@@ -64,7 +64,7 @@ export default function AppDock({
     {
       Icon: WifiIcon,
       title: "Top Up",
-      href: "/bills/internet",
+      href: "/top-up",
       disabled: true,
     },
     {
